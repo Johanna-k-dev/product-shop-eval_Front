ad500f17ae7cca5e00fc6fee468181df5eb1e84c
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-05-27 01:00:39 +0200

    Added new page route on router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,10 +2,13 @@ import {FC} from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 import LayoutConnected from "../layout/LayoutConnected";
 import ProductDetails from "../pages/body/ProductDetails";
-import productsData from "../../src/dataFake/product-data.json";
 import Cart from "../pages/body/Cart";
 import Admin from "../pages/body/Admin";
-import HomePage from "../pages/body/HomePage"; // Importe ta page d'accueil
+import HomePage from "../pages/body/home-page/HomePage";
+import AllProductsPage from "../pages/body/AllProductsPage";
+import SkinCarePage from "../pages/body/SkinCarePage";
+import HairesCarePage from "../pages/body/HairsCarePage";
+import MakeUpPage from "../pages/body/MakeUpPage"; // Importe ta page d'accueil
 
 const Router: FC = () => {
 
@@ -13,10 +16,14 @@ const Router: FC = () => {
         <Routes>
             <Route path="/" element={<LayoutConnected/>}>
                 <Route index element={<Navigate to="/home_page"/>}/>
-                <Route path="/home_page" element={<HomePage/>}/> {/* Ajoute la page d'accueil */}
-                <Route path="/product-details/:id" element={<ProductDetails/>}/>
+                <Route path="/home_page" element={<HomePage/>}/>
+                <Route path={"all-products"} element={<AllProductsPage/>}/>
+                <Route path={"/skin-care"} element={<SkinCarePage/>}/>
+                <Route path={"/hairs-care"} element={<HairesCarePage/>}/>
+                <Route path={"/make-up"} element={<MakeUpPage/>}/>
                 <Route path="/cart" element={<Cart/>}/>
                 <Route path="/admin" element={<Admin/>}/>
+                <Route path="/product-details/:id" element={<ProductDetails/>}/>
             </Route>
         </Routes>
     );
