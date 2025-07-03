8c1eace76f5a41ae470a19b7abf7e85fd22b96d9
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-07-03 10:00:12 +0200

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {BrowserRouter,} from "react-router";
 import './App.css';
 import {AddCartProvider} from "./contexts/AddCartContext";
-import {AuthProvider} from './contexts/AuthContext';
 import AppRoutes from "./router/AppRoutes";
 
 function App() {
