0b148b1ab0d15fc9b42372f033cd7a6b074dff20
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-07-03 12:43:11 +0200

    Added link in footer

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -3,8 +3,15 @@ import './Footer.css'
 
 const Footer: FC<{}> = ({}) => {
     return (
-        <footer className={"footer"}>
-
+        <footer className={"footer"} style={{padding:'30px' , display : 'flex'}}>
+<ul>
+    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/rediger-des-conditions-generales-dutilisation' style={{textDecoration:'none', color:'black', cursor:'cursorPointer'}}>CGU</a></li>
+    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-e-commerce/rediger-des-conditions-generales-de'
+           style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>CGV</a></li>
+    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/quelles-sont-les-mentions-legales-pour-un-site'
+           style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>Mentions légales</a></li>
+    <li><a href="#" style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>Contact</a></li>
+</ul>
         </footer>
     );
 };
