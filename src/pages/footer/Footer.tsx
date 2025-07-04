13492a2c77b59cde431d181a4f989098f823f4c8
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-07-04 11:22:22 +0200

    Added link in footer

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -4,14 +4,13 @@ import './Footer.css'
 const Footer: FC<{}> = ({}) => {
     return (
         <footer className={"footer"} style={{padding:'30px' , display : 'flex'}}>
-<ul>
-    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/rediger-des-conditions-generales-dutilisation' style={{textDecoration:'none', color:'black', cursor:'cursorPointer'}}>CGU</a></li>
-    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-e-commerce/rediger-des-conditions-generales-de'
-           style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>CGV</a></li>
-    <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/quelles-sont-les-mentions-legales-pour-un-site'
-           style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>Mentions légales</a></li>
-    <li><a href="#" style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>Contact</a></li>
-</ul>
+        <ul>
+            <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/rediger-des-conditions-generales-dutilisation' style={{textDecoration:'none', color:'black', cursor:'cursorPointer'}}>CGU</a></li>
+            <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-e-commerce/rediger-des-conditions-generales-de'
+                   style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>CGV</a></li>
+            <li><a href='https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/quelles-sont-les-mentions-legales-pour-un-site'
+                   style={{textDecoration: 'none', color: 'black', cursor: 'cursorPointer'}}>Mentions légales</a></li>
+        </ul>
         </footer>
     );
 };
